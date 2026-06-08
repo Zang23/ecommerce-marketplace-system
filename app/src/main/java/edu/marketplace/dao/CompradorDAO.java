85d9ef5f9a78bbfa0d030f31738f8c22a5d80b26
Zang_23 <thiagoaraujo20005@gmail.com>
2026-06-07 22:14:34 -0300

    Modificacoes feitas: @Zang_23
    	-Implementando compradorDAO (Inserir, Atualizar, Deletar)
    
    Modificacoes a fazer:
    
    	-Implemtar o funcao para buscar comprador por Email e Senha

--- app/src/main/java/edu/marketplace/dao/CompradorDAO.java
+++ app/src/main/java/edu/marketplace/dao/CompradorDAO.java
@@ -1,12 +1,11 @@
 package edu.marketplace.dao;
 
-import java.sql.SQLException;
-
 import edu.marketplace.entity.Comprador;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
-public class CompradorDAO {
+public class CompradorDAO implements CrudDAO<Comprador> {
 
   private Connection con;
 
@@ -14,17 +13,108 @@ public class CompradorDAO {
     this.con = con;
   }
 
- public void inserir (){
-   
+
+@Override
+ public boolean inserir (Comprador c){
+
+      String sql = """
+        INSERT INTO comprador
+          (email, senha, nome, telefone, endereco_logradouro, endereco_numero, endereco_cep, endereco_cidade)
+        VALUES 
+          (?, ?, ?, ?, ?, ?, ?, ?)
+        """;
+
+
+    try(PreparedStatement stm = con.prepareStatement(sql)){
+
+      stm.setString(1, c.getEmail());
+      stm.setString(2, c.getSenha());
+      stm.setString(3, c.getNome());
+      stm.setString(4, c.getTelefone());
+      stm.setString(5, c.getEnderecoLogradouro());
+      stm.setInt(6, c.getEnderecoNumero());
+      stm.setString(7, c.getEnderecoCep());
+      stm.setString(8, c.getEnderecoCidade());
+
+      stm.executeUpdate();
+
+      return true;
+      
+    } catch (Exception e) {
+      System.out.println("Erro ao inserir comprador: " + e.getMessage());
+      return false;
+    }
+
  }
 
-  public void atualizar(){
+  @Override
+  public boolean atualizar(Comprador c){
+    
+    String sql = """
+      UPDATE comprador
+      SET 
+        email = ?,
+        senha = ?,
+        nome = ?,
+        telefone = ?,
+        endereco_logradouro = ?
+        endereco_numero = ?
+        endereco_cep = ?
+        endereco_cidade = ?
+      WHERE id = ?
+    """;
 
+    try(PreparedStatement stm = con.prepareStatement(sql)){
+
+      stm.setString(1, c.getEmail());
+      stm.setString(2, c.getSenha());
+      stm.setString(3, c.getNome());
+      stm.setString(4, c.getTelefone());
+      stm.setString(5, c.getEnderecoLogradouro());
+      stm.setInt(6, c.getEnderecoNumero());
+      stm.setString(7, c.getEnderecoCep());
+      stm.setString(8, c.getEnderecoCidade());
+      stm.setLong(9, c.getId());
+
+      stm.executeUpdate();
+
+      return true;
+
+    }catch(Exception e){
+      System.out.println("Erro ao atualizar comprador: " + e.getMessage());
+      return false;
+    }
+    
   }
 
-  public void excluir(){
+  @Override
+  public boolean excluir(Long id){
+    
+    String sql = """
+      DELETE FROM comprador
+      WHERE id = ?
+    """;
 
+    try(PreparedStatement stm = con.prepareStatement(sql)){
+        
+      stm.setLong(1, id);
+
+      stm.executeUpdate();
+
+      return true;
+
+    }catch(Exception e){
+      System.out.println("Erro ao deletar comprador: {" + id + "}" + e.getMessage());
+    }
+
+    return false;
   }
+
+  @Override 
+  public Comprador buscarPorId(Long id){
+    return null;
+  }
+  
 
   
 
